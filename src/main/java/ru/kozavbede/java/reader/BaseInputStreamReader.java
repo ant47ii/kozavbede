7e9e79c754b4d3919f47df280636169b96a7e509
tolik <tolik@DESKTOP-MFDA055>
2020-12-26 17:49:07 +0700

    Поддержка чтения байтов.

---
+++ src/main/java/ru/kozavbede/java/reader/BaseInputStreamReader.java
@@ -0,0 +1,56 @@
+package ru.kozavbede.java.reader;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 
+ * Базовая реализация чтения {@code InputStream}.
+ *
+ * @param <T> Тип, возвращаемый при чтении потока.
+ */
+public abstract class BaseInputStreamReader<T> implements IByteReader {
+
+	private final InputStream is;
+
+	protected BaseInputStreamReader(InputStream is) {
+		this.is = is;
+	}
+
+	public abstract T read() throws IOException;
+
+	@Override
+	public int read4Int() throws IOException {
+		byte[] bytes = readNBytes(4);
+		return read(bytes);
+	}
+
+	@Override
+	public int read2Int() throws IOException {
+		byte[] bytes = readNBytes(2);
+		return read(bytes);
+	}
+
+	@Override
+	public int read1Int() throws IOException {
+		byte[] bytes = readNBytes(1);
+		return read(bytes);
+	}
+
+	@Override
+	public byte[] readNBytes(int len) throws IOException {
+		return is.readNBytes(len);
+	}
+
+	private int read(byte[] bytes) {
+		int result = 0;
+		for (int i = 0; i < bytes.length; i++) {
+			result |= read(bytes[bytes.length - i - 1], i * 8);
+		}
+		return result;
+	}
+
+	private int read(byte b, int position) {
+		return Byte.toUnsignedInt(b) << position;
+	}
+}
